f24aaad8a8394b7305ade26c2819f404f58b0f52
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-10-21 17:48:53 +0300

    Add files via upload

---
+++ Students/Sums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    
+    public class Sums
+    {
+        public SumDelegate PaymentSum { get; } = (Student student) => student.Paid;
+        public SumDelegate NeedToPaySum { get; } = (Student student) => student.NeedToPay;
+    }
+}
